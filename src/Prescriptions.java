0eeb06973ba10a31bc4436b97838274dfd2b74a2
Santiago Marriaga <Turtles@santiagos-mbp-2.home>
2018-11-30 21:53:57 -0500

    Other stuff

--- src/Prescriptions.java
+++ src/Prescriptions.java
@@ -5,4 +5,75 @@ public class Prescriptions {
 	private String dateIssued;
 	private String doctor;
 	private ArrayList<Druglines> drugLines;
+	
+	/**
+	 * @param id
+	 * @param dateIssued
+	 * @param doctor
+	 * @param drugLines
+	 */
+	public Prescriptions(String id, String dateIssued, String doctor, ArrayList<Druglines> drugLines) {
+		this.id = id;
+		this.dateIssued = dateIssued;
+		this.doctor = doctor;
+		this.drugLines = drugLines;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the dateIssued
+	 */
+	public String getDateIssued() {
+		return dateIssued;
+	}
+
+	/**
+	 * @param dateIssued the dateIssued to set
+	 */
+	public void setDateIssued(String dateIssued) {
+		this.dateIssued = dateIssued;
+	}
+
+	/**
+	 * @return the doctor
+	 */
+	public String getDoctor() {
+		return doctor;
+	}
+
+	/**
+	 * @param doctor the doctor to set
+	 */
+	public void setDoctor(String doctor) {
+		this.doctor = doctor;
+	}
+
+	/**
+	 * @return the drugLines
+	 */
+	public ArrayList<Druglines> getDrugLines() {
+		return drugLines;
+	}
+
+	/**
+	 * @param drugLines the drugLines to set
+	 */
+	public void setDrugLines(ArrayList<Druglines> drugLines) {
+		this.drugLines = drugLines;
+	}
+	
+	
 }
